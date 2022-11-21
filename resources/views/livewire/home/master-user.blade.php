81f1df61fd7187ba2add711c4b13c65721a60a24
“Agung <agungpriyatno666@email.com>
2022-11-21 11:31:56 +0700

    update

--- resources/views/livewire/home/master-user.blade.php
+++ resources/views/livewire/home/master-user.blade.php
@@ -3,10 +3,7 @@
     <div class="container-fluid">
 
         <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-        <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-            For more information about DataTables, please visit the <a target="_blank"
-                href="https://datatables.net">official DataTables documentation</a>.</p>
+        <h1 class="h3 text-gray-800 mb-4">Data User</h1>
 
         @if (session()->has('error'))
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
