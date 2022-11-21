25dd517a426bcb3ed1fed5250f800a19fdeb8384
“Agung <agungpriyatno666@email.com>
2022-11-21 10:34:45 +0700

    update

--- resources/views/partials/home/footer.blade.php
+++ resources/views/partials/home/footer.blade.php
@@ -2,8 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
-        </div>
+            <span>Copyright &copy; Speed Control
     </div>
 </footer>
 <!-- End of Footer -->
